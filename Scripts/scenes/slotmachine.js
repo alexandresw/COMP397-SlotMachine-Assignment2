1366a6d9689e3439e389385dcb319a4991842f20
Alexandre Watanabe <alexandresw@gmail.com>
2016-02-21 23:02:56 -0500

    added game logic functions

--- Scripts/scenes/slotmachine.js
+++ Scripts/scenes/slotmachine.js
@@ -11,14 +11,6 @@ var scenes;
         // CONSTRUCTOR ++++++++++++++++++++++
         function SlotMachine() {
             _super.call(this);
-            this._captain = 0;
-            this._ironman = 0;
-            this._wolverine = 0;
-            this._spiderman = 0;
-            this._hulk = 0;
-            this._batman = 0;
-            this._superman = 0;
-            this._blanks = 0;
         }
         // PUBLIC METHODS +++++++++++++++++++++
         // Start Method
@@ -63,6 +55,7 @@ var scenes;
             this._setupBackground("WhiteBackground");
             // FadeIn
             this._fadeIn(500);
+            this._initialize();
             // add this scene to the global stage container
             stage.addChild(this);
         };
@@ -70,6 +63,87 @@ var scenes;
         SlotMachine.prototype.update = function () {
         };
         //PRIVATE METHODS
+        /* Initialize game variables */
+        SlotMachine.prototype._initialize = function () {
+            this._playerMoney = 1000;
+            this._winnings = 0;
+            this._jackpot = 5000;
+            this._turn = 0;
+            this._playerBet = 0;
+            this._winNumber = 0;
+            this._lossNumber = 0;
+            this._captain = 0;
+            this._ironman = 0;
+            this._wolverine = 0;
+            this._spiderman = 0;
+            this._hulk = 0;
+            this._batman = 0;
+            this._superman = 0;
+            this._blanks = 0;
+            // set initial position
+            var result = this._reels();
+            this._reel1.setHero(result[0]);
+            this._reel2.setHero(result[1]);
+            this._reel3.setHero(result[2]);
+        };
+        /* Utility function to reset all Heroes tallies */
+        SlotMachine.prototype._resetHeroesTally = function () {
+            this._captain = 0;
+            this._ironman = 0;
+            this._wolverine = 0;
+            this._spiderman = 0;
+            this._hulk = 0;
+            this._batman = 0;
+            this._superman = 0;
+            this._blanks = 0;
+        };
+        /* Utility function to reset the player stats */
+        SlotMachine.prototype._resetAll = function () {
+            this._playerMoney = 1000;
+            this._winnings = 0;
+            this._jackpot = 5000;
+            this._turn = 0;
+            this._playerBet = 0;
+            this._winNumber = 0;
+            this._lossNumber = 0;
+            //this._winRatio = 0;
+        };
+        SlotMachine.prototype._showPlayerStats = function () {
+            console.log("------RESULTS------");
+            // winRatio = winNumber / turn;
+            console.log("Jackpot: " + this._jackpot);
+            console.log("Player Bet: " + this._playerBet);
+            console.log("Player Money: " + this._playerMoney);
+            console.log("Turn: " + this._turn);
+            console.log("Wins: " + this._winNumber);
+            console.log("Losses: " + this._lossNumber);
+            //console.log("Win Ratio: " + (winRatio * 100).toFixed(2) + "%");
+            console.log("--------------------");
+        };
+        /* Check to see if the player won the jackpot */
+        SlotMachine.prototype._checkJackPot = function () {
+            /* compare two random values */
+            var jackPotTry = Math.floor(Math.random() * 51 + 1);
+            var jackPotWin = Math.floor(Math.random() * 51 + 1);
+            if (jackPotTry == jackPotWin) {
+                alert("You Won the $" + this._jackpot + " Jackpot!!");
+                this._playerMoney += this._jackpot;
+                this._jackpot = 1000;
+            }
+        };
+        /* Utility function to show a win message and increase player money */
+        SlotMachine.prototype._showWinMessage = function () {
+            this._playerMoney += this._winnings;
+            console.log("You Won: $" + this._winnings);
+            this._resetHeroesTally();
+            this._checkJackPot();
+        };
+        /* Utility function to show a loss message and reduce player money */
+        SlotMachine.prototype._showLossMessage = function () {
+            this._playerMoney -= this._playerBet;
+            console.log("You Lost!");
+            this._resetHeroesTally();
+        };
         /* Utility function to check if a value falls within a range of bounds */
         SlotMachine.prototype._checkRange = function (value, lowerBounds, upperBounds) {
             return (value >= lowerBounds && value <= upperBounds) ? value : -1;
@@ -118,29 +192,118 @@ var scenes;
             }
             return betLine;
         };
+        /* This function calculates the player's winnings, if any */
+        SlotMachine.prototype._determineWinnings = function () {
+            if (this._blanks == 0) {
+                if (this._captain == 3) {
+                    this._winnings = this._playerBet * 10;
+                }
+                else if (this._ironman == 3) {
+                    this._winnings = this._playerBet * 20;
+                }
+                else if (this._wolverine == 3) {
+                    this._winnings = this._playerBet * 30;
+                }
+                else if (this._spiderman == 3) {
+                    this._winnings = this._playerBet * 40;
+                }
+                else if (this._hulk == 3) {
+                    this._winnings = this._playerBet * 50;
+                }
+                else if (this._batman == 3) {
+                    this._winnings = this._playerBet * 75;
+                }
+                else if (this._superman == 3) {
+                    this._winnings = this._playerBet * 100;
+                }
+                else if (this._captain == 2) {
+                    this._winnings = this._playerBet * 2;
+                }
+                else if (this._ironman == 2) {
+                    this._winnings = this._playerBet * 2;
+                }
+                else if (this._wolverine == 2) {
+                    this._winnings = this._playerBet * 3;
+                }
+                else if (this._spiderman == 2) {
+                    this._winnings = this._playerBet * 4;
+                }
+                else if (this._hulk == 2) {
+                    this._winnings = this._playerBet * 5;
+                }
+                else if (this._batman == 2) {
+                    this._winnings = this._playerBet * 10;
+                }
+                else if (this._superman == 2) {
+                    this._winnings = this._playerBet * 20;
+                }
+                else if (this._superman == 1) {
+                    this._winnings = this._playerBet * 5;
+                }
+                else {
+                    this._winnings = this._playerBet * 1;
+                }
+                this._winNumber++;
+                this._showWinMessage();
+            }
+            else {
+                this._lossNumber++;
+                this._showLossMessage();
+            }
+        };
         //EVENT HANDLERS ++++++++++++++++++++
         SlotMachine.prototype._bet1ButtonClick = function (event) {
             console.log("Bet 1 Credit");
+            this._playerBet = 1;
         };
         SlotMachine.prototype._bet10ButtonClick = function (event) {
             console.log("Bet 10 Credit");
+            this._playerBet = 10;
         };
         SlotMachine.prototype._bet100ButtonClick = function (event) {
             console.log("Bet 100 Credit");
+            this._playerBet = 100;
         };
         SlotMachine.prototype._resetButtonClick = function (event) {
             console.log("Reset Game!");
+            // Restart Scene
+            scene = config.Scene.SLOT_MACHINE;
+            changeScene();
         };
         SlotMachine.prototype._quitButtonClick = function (event) {
             console.log("Quit Game!");
+            // Switch to MENU Scene
+            scene = config.Scene.MENU;
+            changeScene();
         };
         SlotMachine.prototype._spinButtonClick = function (event) {
             console.log("Spin those reels!");
-            var result = this._reels();
-            console.log("result=" + result);
-            this._reel1.setHero(result[0]);
-            this._reel2.setHero(result[1]);
-            this._reel3.setHero(result[2]);
+            if (this._playerMoney == 0) {
+                if (confirm("You ran out of Money! \nDo you want to play again?")) {
+                    this._resetAll();
+                    this._showPlayerStats();
+                }
+            }
+            else if (this._playerBet > this._playerMoney) {
+                alert("You don't have enough Money to place that bet.");
+            }
+            else if (this._playerBet < 0) {
+                alert("All bets must be a positive $ amount.");
+            }
+            else if (this._playerBet <= this._playerMoney) {
+                var spinResult = this._reels();
+                //fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+                //$("div#result>p").text(fruits);
+                this._reel1.setHero(spinResult[0]);
+                this._reel2.setHero(spinResult[1]);
+                this._reel3.setHero(spinResult[2]);
+                this._determineWinnings();
+                this._turn++;
+                this._showPlayerStats();
+            }
+            else {
+                alert("Please enter a valid bet amount");
+            }
             //this._test();
         };
         SlotMachine.prototype._test = function () {
--- Scripts/scenes/slotmachine.ts
+++ Scripts/scenes/slotmachine.ts
@@ -14,14 +14,21 @@ module scenes {
         private _reel2: objects.Reel;
         private _reel3: objects.Reel;
 
-        private _captain = 0; 
-        private _ironman = 0;
-        private _wolverine = 0;
-        private _spiderman = 0;
-        private _hulk = 0;
-        private _batman = 0;
-        private _superman = 0;
-        private _blanks = 0;
+        private _playerMoney: number;
+        private _winnings: number;
+        private _jackpot: number;
+        private _turn: number;
+        private _playerBet: number;
+        private _winNumber: number;
+        private _lossNumber: number;
+        private _captain: number; 
+        private _ironman: number;
+        private _wolverine: number;
+        private _spiderman: number;
+        private _hulk: number;
+        private _batman: number;
+        private _superman: number;
+        private _blanks: number;
         // CONSTRUCTOR ++++++++++++++++++++++
         constructor() {
             super();
@@ -35,7 +42,6 @@ module scenes {
             this._backgroundImage = new createjs.Bitmap(assets.getResult("SlotMachine"));
             this.addChild(this._backgroundImage);
             
-            
             // create reel 1
             this._reel1 = new objects.Reel(190, 246);
             this.addChild(this._reel1);
@@ -47,7 +53,6 @@ module scenes {
             // create reel 3
             this._reel3 = new objects.Reel(430, 246);
             this.addChild(this._reel3);
-            
             
             // add Bet1Button to the scene
             this._bet1Button = new objects.Button("Bet1Button", 205, 436, false);
@@ -84,6 +89,8 @@ module scenes {
            
             // FadeIn
             this._fadeIn(500);
+            
+            this._initialize();
         
             // add this scene to the global stage container
             stage.addChild(this);
@@ -94,7 +101,98 @@ module scenes {
 
         }
         
+        
         //PRIVATE METHODS
+        
+        /* Initialize game variables */
+        private _initialize(): void {
+            this._playerMoney = 1000;
+            this._winnings = 0;
+            this._jackpot = 5000;
+            this._turn = 0;
+            this._playerBet = 0;
+            this._winNumber = 0;
+            this._lossNumber = 0;
+            this._captain = 0;
+            this._ironman = 0;
+            this._wolverine = 0;
+            this._spiderman = 0;
+            this._hulk = 0;
+            this._batman = 0;
+            this._superman = 0;
+            this._blanks = 0;
+            
+            // set initial position
+            var result = this._reels();
+            this._reel1.setHero(result[0]);
+            this._reel2.setHero(result[1]);
+            this._reel3.setHero(result[2]);
+        }
+        
+        /* Utility function to reset all Heroes tallies */
+        private _resetHeroesTally() {
+            this._captain = 0;
+            this._ironman = 0;
+            this._wolverine = 0;
+            this._spiderman = 0;
+            this._hulk = 0;
+            this._batman = 0;
+            this._superman = 0;
+            this._blanks = 0;
+        }
+
+        /* Utility function to reset the player stats */
+        private _resetAll() {
+            this._playerMoney = 1000;
+            this._winnings = 0;
+            this._jackpot = 5000;
+            this._turn = 0;
+            this._playerBet = 0;
+            this._winNumber = 0;
+            this._lossNumber = 0;
+            //this._winRatio = 0;
+        }
+        
+        private _showPlayerStats(): void {
+            console.log("------RESULTS------");
+            // winRatio = winNumber / turn;
+            console.log("Jackpot: " + this._jackpot);
+            console.log("Player Bet: " + this._playerBet);
+            console.log("Player Money: " + this._playerMoney);
+            console.log("Turn: " + this._turn);
+            console.log("Wins: " + this._winNumber);
+            console.log("Losses: " + this._lossNumber);
+            //console.log("Win Ratio: " + (winRatio * 100).toFixed(2) + "%");
+            console.log("--------------------");
+        }
+        
+        /* Check to see if the player won the jackpot */
+        private _checkJackPot(): void {
+            /* compare two random values */
+            var jackPotTry = Math.floor(Math.random() * 51 + 1);
+            var jackPotWin = Math.floor(Math.random() * 51 + 1);
+            if (jackPotTry == jackPotWin) {
+                alert("You Won the $" + this._jackpot + " Jackpot!!");
+                this._playerMoney += this._jackpot;
+                this._jackpot = 1000;
+            }
+        }
+        
+        /* Utility function to show a win message and increase player money */
+        private _showWinMessage() {
+            this._playerMoney += this._winnings;
+            console.log("You Won: $" + this._winnings);
+            this._resetHeroesTally();
+            this._checkJackPot();
+        }
+
+        /* Utility function to show a loss message and reduce player money */
+        private _showLossMessage() {
+            this._playerMoney -= this._playerBet;
+            console.log("You Lost!");
+            this._resetHeroesTally();
+        }
+        
         /* Utility function to check if a value falls within a range of bounds */
         private _checkRange(value: number, lowerBounds: number, upperBounds: number): number {
             return (value >= lowerBounds && value <= upperBounds) ? value : -1;
@@ -146,35 +244,131 @@ module scenes {
             return betLine;
         }
         
+        /* This function calculates the player's winnings, if any */
+        private _determineWinnings(): void {
+            if (this._blanks == 0)
+            {
+                if (this._captain == 3) {
+                    this._winnings = this._playerBet * 10;
+                }
+                else if(this._ironman == 3) {
+                    this._winnings = this._playerBet * 20;
+                }
+                else if (this._wolverine == 3) {
+                    this._winnings = this._playerBet * 30;
+                }
+                else if (this._spiderman == 3) {
+                    this._winnings = this._playerBet * 40;
+                }
+                else if (this._hulk == 3) {
+                    this._winnings = this._playerBet * 50;
+                }
+                else if (this._batman == 3) {
+                    this._winnings = this._playerBet * 75;
+                }
+                else if (this._superman == 3) {
+                    this._winnings = this._playerBet * 100;
+                }
+                else if (this._captain == 2) {
+                    this._winnings = this._playerBet * 2;
+                }
+                else if (this._ironman == 2) {
+                    this._winnings = this._playerBet * 2;
+                }
+                else if (this._wolverine == 2) {
+                    this._winnings = this._playerBet * 3;
+                }
+                else if (this._spiderman == 2) {
+                    this._winnings = this._playerBet * 4;
+                }
+                else if (this._hulk == 2) {
+                    this._winnings = this._playerBet * 5;
+                }
+                else if (this._batman == 2) {
+                    this._winnings = this._playerBet * 10;
+                }
+                else if (this._superman == 2) {
+                    this._winnings = this._playerBet * 20;
+                }
+                else if (this._superman == 1) {
+                    this._winnings = this._playerBet * 5;
+                }
+                else {
+                    this._winnings = this._playerBet * 1;
+                }
+                this._winNumber++;
+                this._showWinMessage();
+            }
+            else
+            {
+                this._lossNumber++;
+                this._showLossMessage();
+            }
+            
+        }
+        
         //EVENT HANDLERS ++++++++++++++++++++
         private _bet1ButtonClick(event: createjs.MouseEvent): void {
             console.log("Bet 1 Credit");
+            this._playerBet = 1;
         }
 
         private _bet10ButtonClick(event: createjs.MouseEvent): void {
             console.log("Bet 10 Credit");
+            this._playerBet = 10;
         }
 
         private _bet100ButtonClick(event: createjs.MouseEvent): void {
             console.log("Bet 100 Credit");
+            this._playerBet = 100;
         }
         
         private _resetButtonClick(event: createjs.MouseEvent): void {
             console.log("Reset Game!");
+            // Restart Scene
+            scene = config.Scene.SLOT_MACHINE;
+            changeScene();
         }
         
         private _quitButtonClick(event: createjs.MouseEvent): void {
             console.log("Quit Game!");
+            // Switch to MENU Scene
+            scene = config.Scene.MENU;
+            changeScene();
         }
 
         private _spinButtonClick(event: createjs.MouseEvent): void {
             console.log("Spin those reels!");
-            var result = this._reels();
-            console.log("result="+result);
             
-            this._reel1.setHero(result[0]);
-            this._reel2.setHero(result[1]);
-            this._reel3.setHero(result[2]);
+            if (this._playerMoney == 0) {
+                if (confirm("You ran out of Money! \nDo you want to play again?")) {
+                    this._resetAll();
+                    this._showPlayerStats();
+                }
+            }
+            else if (this._playerBet > this._playerMoney) {
+                alert("You don't have enough Money to place that bet.");
+            }
+            else if (this._playerBet < 0) {
+                alert("All bets must be a positive $ amount.");
+            }
+            else if (this._playerBet <= this._playerMoney) {
+                var spinResult = this._reels();
+                //fruits = spinResult[0] + " - " + spinResult[1] + " - " + spinResult[2];
+                //$("div#result>p").text(fruits);
+                this._reel1.setHero(spinResult[0]);
+                this._reel2.setHero(spinResult[1]);
+                this._reel3.setHero(spinResult[2]);
+            
+                this._determineWinnings();
+                this._turn++;
+                this._showPlayerStats();
+            }
+            else {
+                alert("Please enter a valid bet amount");
+            }
+            
+            
             //this._test();
         }
         
